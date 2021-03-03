c1aa81b5b78fcd639fa83ee259ff7cc629d5446d
uranusack CHANTHAVISOUK <uranusack.c@gmail.com>
2021-03-03 14:40:56 +0100

    projet fini

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,27 +5,32 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
+	public void calculateFare(Ticket ticket){
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
-
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
-
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
-        }
-    }
+		if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ) {
+			throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+		}
+		
+		double inTime = ticket.getInTime().getTime();
+		double outTime = ticket.getOutTime().getTime();
+		
+		double duration = (outTime - inTime) / (60*60*1000);
+		
+		if (duration > 0.5)
+		{
+			switch (ticket.getParkingSpot().getParkingType()) {
+				case CAR: {
+					ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+					break;
+				}
+				case BIKE: {
+					ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+					break;
+				}
+				default: throw new IllegalArgumentException("Unkown Parking Type");
+			}
+		}
+		else
+			ticket.setPrice(0);
+	}
 }
